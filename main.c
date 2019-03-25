c95ca05f5ad2506460542e7fae410a873c60b7bd
oettam000 <licheri11@gmail.com>
2019-03-25 19:36:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 
 int main() {
     //qua inserisco il numero che verra generato random e lo faro prendere in ingresso alle funzioni in modo da non incasinarmi in futuro
-
-    int random=1-rand()%6-1;
-    struct giocatore *G;//creazione della lista giocatori
+    srand(time(NULL));
+    int random=1+rand()%GIOCATORI;
     struct Lista *MAZZO;
-    struct tabellone *T;
-    G=generazione_giocatori();//funziona
+    struct tabellone *A;
     MAZZO=generazione_mazzo();//funziona
-    T=fase_pre_gioco(G,random,MAZZO);
-    //T=fase_pre_gioco(G,random,MAZZO);//non mi ritorna zero
+    MAZZO=mescola(MAZZO);
+    //visualizza(MAZZO);
+    generazione_giocatori();//assegna il numero delle truppe a i rispettivi giocatori
+    //gioca();//mi restituisce il nome del giocatore
+    A=fase_pre_gioco(random,MAZZO);//mi da un nome sbagliato all secondo inserimento della truppa
+    A=Proprietario(A,random);
+    //tabelloneVIS(A);
+
+    schieramento(A,random);
     return 0;
 }
